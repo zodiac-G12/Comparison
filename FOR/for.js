e067907b442551e81cc5a453fc2dde4284fa2117
zodiac-G12 <zodiac@zodiac-ThinkPad-E450>
2017-07-29 01:38:25 +0900

    js

--- FOR/for.js
+++ FOR/for.js
@@ -1,16 +1,27 @@
 console.time('timer');
 len = 100000001;
 arr = new Array(len);
-i = 0;
+var i = 0;
 while(i < len){
   arr[i] = i;
-  i = (i+1);//|0;
+  i = (i+1)|0;
 }
+// i = 0;
+// while(i < len){
+//   arr[i] = i;
+//   i = (i+1)|0;
+// }
+// i = 0;
+// sum = 0;
+// while(i < arr.length){
+//   sum = (sum+arr[i])|0;
+//   i=(i+1)|0;
+// }
 i = 0;
 sum = 0;
-while(i < arr.length){
-   sum = sum+arr[i];//(sum+arr[i])|0;
-   i=(i+1);//|0;
+while(i < len){
+  sum += arr[i]|0; //ココを変えます
+  i = (i+1)|0;
 }
-console.log(sum)
+console.log(sum);
 console.timeEnd('timer');
